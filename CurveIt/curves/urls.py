c324f5fc2cc8a678c75cca3cd0885acbfe5b08bd
Advait Chauhan <achauhan@princeton.edu>
2015-03-29 11:13:16 -0400

    hyperlinks added to dept view

--- CurveIt/curves/urls.py
+++ CurveIt/curves/urls.py
@@ -9,7 +9,9 @@ urlpatterns = patterns('',
 	url(r'^(?P<cdept>[A-Za-z]{3,3})/$', views.deptView, name = 'deptView'), 
 
 	#class view
+	#will show a class and link to course specifics for all semesters class has been offered
+	#ommitted for now since not critical to rudimentary functionality
 
 	#course specific view
-	url(r'^(?P<cdept>[A-Za-z]{3,3})/(?P<cnum>\d{3,3})/(?P<ctime>(F|S)\d{4,4})$', views.courseSpecificView, name = 'courseSpecificView')
+	url(r'^(?P<cdept>[A-Za-z]{3,3})/(?P<cnum>\d{3,3})/(?P<ctime>(F|S)\d{4,4})/$', views.courseSpecificView, name = 'courseSpecificView')
 )
