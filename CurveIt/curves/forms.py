3413b03ce20891d00b631078cfd8a37758b0aff2
Advait Chauhan <achauhan@princeton.edu>
2015-04-08 23:08:12 -0400

    search form

--- CurveIt/curves/forms.py
+++ CurveIt/curves/forms.py
@@ -16,3 +16,22 @@ class Course_SpecificForm(forms.ModelForm):
         # Provide an association between the ModelForm and a model
         model = Course_Specific
         fields = ('pastSemClass', 'grade')
+
+class SearchForm(forms.Form):
+    curClasses = Course_Specific.objects.filter(semester="S2015")
+    curClassesList = []
+    for j in range (0, len(curClasses)):
+        curClass = (curClasses[j].__unicode__(), curClasses[j].__unicode__())
+        curClassesList.append(curClass)
+    curClassesList.sort()
+    curProfs = []
+    curDepts = []
+    for class in Course_Specific.objects.all():
+        if class.prof not in curProfs:
+            curProfs.append(class.prof)
+        if class.dept not in curDepts:
+            curDepts.append(class.dept)
+    curProfs.sort()
+    curDepts.sort()
+    allChoices = curClassesList + curProfs + curDepts
+    search = forms.ChoiceField(choices = allChoices, help_text = "Class, Dept, or Professor")
