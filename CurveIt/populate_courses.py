125429adcc24e166c1469e5f6267e12fede36b38
Tyler <tylerh@princeton.edu>
2015-04-13 08:59:11 -0400

    Populate now with different grades (for Adi)

--- CurveIt/populate_courses.py
+++ CurveIt/populate_courses.py
@@ -56,7 +56,7 @@ def main():
       else:
         thisNum += theseNums[i] + "+"    
     thisTitle = course["title"]
-    courseSpecifics.append(Course_Specific(dept=thisDept, num=thisNum, name=thisTitle, prof=thisProf, semester="S2015", num_A_plus=20, num_A=30, num_A_minus=35, num_B_plus=20, num_B=11, num_B_minus=8, num_C_plus=4))
+    courseSpecifics.append(Course_Specific(dept=thisDept, num=thisNum, name=thisTitle, prof=thisProf, semester="S2015", num_A_plus=20, num_A=30, num_A_minus=35, num_B_plus=20, num_B=11, num_B_minus=8, num_C_plus=4, num_C_minus=8))
 
   for i in range(0, len(courseSpecifics)):
       courseSpecifics[i].save()
@@ -105,7 +105,7 @@ def main():
       else:
         thisNum += theseNums[i] + "+"    
     thisTitle = course["title"]
-    courseSpecifics.append(Course_Specific(dept=thisDept, num=thisNum, name=thisTitle, prof=thisProf, semester="S2014", num_A_plus=20, num_A=30, num_A_minus=35, num_B_plus=20, num_B=11, num_B_minus=8, num_C_plus=4))
+    courseSpecifics.append(Course_Specific(dept=thisDept, num=thisNum, name=thisTitle, prof=thisProf, semester="S2014", num_A_plus=1, num_A=9, num_A_minus=26, num_B_plus=20, num_B=28, num_B_minus=29, num_C_plus=35, num_C=20, num_C_minus=19, num_D_grade=18, num_F_grade=10))
 
   for i in range(0, len(courseSpecifics)):
       courseSpecifics[i].save()
@@ -154,7 +154,7 @@ def main():
       else:
         thisNum += theseNums[i] + "+"    
     thisTitle = course["title"]
-    courseSpecifics.append(Course_Specific(dept=thisDept, num=thisNum, name=thisTitle, prof=thisProf, semester="S2013", num_A_plus=20, num_A=30, num_A_minus=35, num_B_plus=20, num_B=11, num_B_minus=8, num_C_plus=4))
+    courseSpecifics.append(Course_Specific(dept=thisDept, num=thisNum, name=thisTitle, prof=thisProf, semester="S2013", num_A_plus=10, num_A=9, num_A_minus=43, num_B_plus=40, num_B=50, num_B_minus=27, num_C_plus=8, num_C=19, num_D_grade=16, num_F_grade=7))
 
   for i in range(0, len(courseSpecifics)):
       courseSpecifics[i].save()
