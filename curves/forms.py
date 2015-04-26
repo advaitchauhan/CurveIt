3385a3f50364f88c398b7ef48530a6be6c460591
Tyler <tylerh@princeton.edu>
2015-04-26 12:53:32 -0400

    Small change to error format in add data

--- curves/forms.py
+++ curves/forms.py
@@ -12,11 +12,11 @@ class Course_SpecificForm(forms.Form):
     curClasses = Course_Specific.objects.filter(semester="S2015")
     curGradesList = Course_Specific.CHOICES
     curGradesList.insert(0, ("N/A", "Enter Grade"))
-    pastSemClass1 = forms.ModelChoiceField(queryset = curClasses, widget=autocomplete_light.ChoiceWidget('Course_SpecificAutocomplete'), label = "courseInput", help_text="Class*", required=True)
+    pastSemClass1 = forms.ModelChoiceField(queryset = curClasses, widget=autocomplete_light.ChoiceWidget('Course_SpecificAutocomplete'), label = "courseInput", help_text="Class*", required=False)
     grade1 = forms.ChoiceField(choices=Course_Specific.CHOICES, label = "gradeInput", help_text="Grade*")
-    pastSemClass2 = forms.ModelChoiceField(queryset = curClasses,  widget=autocomplete_light.ChoiceWidget('Course_SpecificAutocomplete'), label = "courseInput", help_text="Class*", required=True)
+    pastSemClass2 = forms.ModelChoiceField(queryset = curClasses,  widget=autocomplete_light.ChoiceWidget('Course_SpecificAutocomplete'), label = "courseInput", help_text="Class*", required=False)
     grade2 = forms.ChoiceField(choices=Course_Specific.CHOICES, label = "gradeInput", help_text="Grade*")
-    pastSemClass3 = forms.ModelChoiceField(queryset = curClasses, widget=autocomplete_light.ChoiceWidget('Course_SpecificAutocomplete'), label = "courseInput", help_text="Class*", required=True)
+    pastSemClass3 = forms.ModelChoiceField(queryset = curClasses, widget=autocomplete_light.ChoiceWidget('Course_SpecificAutocomplete'), label = "courseInput", help_text="Class*", required=False)
     grade3 = forms.ChoiceField(choices=Course_Specific.CHOICES, label = "gradeInput",  help_text="Grade*")
     pastSemClass4 = forms.ModelChoiceField(queryset = curClasses, widget=autocomplete_light.ChoiceWidget('Course_SpecificAutocomplete'), label = "courseInput", help_text="Class*", required=False)
     grade4 = forms.ChoiceField(choices=Course_Specific.CHOICES, label = "gradeInput", help_text="Grade")
@@ -54,8 +54,8 @@ class Course_SpecificForm(forms.Form):
             thisGrade = cleaned_data.get(requiredGrades[i])
             thisClass = cleaned_data.get(requiredClasses[i])
             required_Courses.append(thisClass)
-            if thisGrade == "N/A":
-                self.add_error(requiredGrades[i], "This field is required.")
+            if thisGrade == "N/A" or thisClass == None:
+                self.add_error(requiredGrades[i], "Please enter both a class and a grade")
 
         #grab the optional classes, perform error checking with the grabbed classes
         for i in range(0, len(optionalClasses)):
@@ -66,15 +66,30 @@ class Course_SpecificForm(forms.Form):
                 self.add_error(optionalGrades[i], "Please enter grade.")
             elif thisGrade != "N/A" and thisClass == None:
                 self.add_error(optionalClasses[i], "Please enter class.")
-        for i in range(0, len(required_Courses)):
-            for j in range(0, len(required_Courses)):
+        # for i in range(0, len(required_Courses)):
+        #     for j in range(0, len(required_Courses)):
+        #         if i != j:
+        #             if required_Courses[i] != None and required_Courses[j] != None:
+        #                 if required_Courses[i] == required_Courses[j]:
+        #                     self.add_error(requiredClasses[j], "Please do not select a class more than once.")
+        # for i in range(0, len(optional_Courses)):
+        #     for j in range(0, len(optional_Courses)):
+        #         if i != j:
+        #             if optional_Courses[i] != None and optional_Courses[j] != None:
+        #                 if optional_Courses[i] == optional_Courses[j]:
+        #                     self.add_error(optionalClasses[j], "Please do not select a class more than once")
+        all_Courses = required_Courses + optional_Courses
+        errors = [False]*6
+        # checks that a class has not been selected more than once
+        for i in range(0, len(all_Courses)):
+            for j in range(0, len(all_Courses)):
                 if i != j:
-                    if required_Courses[i] != None and required_Courses[j] != None:
-                        if required_Courses[i] == required_Courses[j]:
-                            self.add_error(requiredClasses[j], "Please do not select a class more than once.")
-        for i in range(0, len(optional_Courses)):
-            for j in range(0, len(optional_Courses)):
-                if i != j:
-                    if optional_Courses[i] != None and optional_Courses[j] != None:
-                        if optional_Courses[i] == optional_Courses[j]:
-                            self.add_error(optionalClasses[j], "Please do not select a class more than once")
+                    if all_Courses[i] != None and all_Courses[j] != None:
+                        if errors[j] == False:
+                            if all_Courses[i] == all_Courses[j]:
+                                if j < len(required_Courses):
+                                    self.add_error(requiredClasses[j], "Please do not select a class more than once.")
+                                    errors[j] = True
+                                else:
+                                    self.add_error(optionalClasses[j - len(required_Courses)], "Please do not select a class more than once.")
+                                    errors[j] = True
