f60dc28898e6d449411145f8be7255991bae7fe5
Tyler <tylerh@princeton.edu>
2015-04-25 21:50:44 -0400

    I reduced duplicates in autocomplete (a problem that Adi could not solve)

--- curves/autocomplete_light_registry.py
+++ curves/autocomplete_light_registry.py
@@ -2,22 +2,36 @@ import autocomplete_light
 from models import Course_Specific
 
 
-autocomplete_light.register(Course_Specific,
-	search_fields = ['titleString', 'prof'],
+class Course_SpecificAutocomplete(autocomplete_light.AutocompleteModelBase):
+    search_fields = ['titleString', 'prof']
     attrs={
     # This will set the input placeholder attribute:
     'placeholder': 'Select a Class',
     # This will set the yourlabs.Autocomplete.minimumCharacters
     # options, the naming conversion is handled by jQuery
     'data-autocomplete-minimum-characters': 1,
-	},
-	
-	# This will set the data-widget-maximum-values attribute on the
-	# widget container element, and will be set to
-	# yourlabs.Widget.maximumValues (jQuery handles the naming
-	# conversion).
-	widget_attrs={
+    }
+    widget_attrs={
 	    # 'data-widget-maximum-values': 4,
 	    'class': 'modern-style',
-    },	
-)
+    }
+autocomplete_light.register(Course_SpecificAutocomplete, choices=Course_Specific.objects.filter(semester="S2015"))
+# autocomplete_light.register(Course_Specific,
+# 	search_fields = ['titleString', 'prof'],
+#     attrs={
+#     # This will set the input placeholder attribute:
+#     'placeholder': 'Select a Class',
+#     # This will set the yourlabs.Autocomplete.minimumCharacters
+#     # options, the naming conversion is handled by jQuery
+#     'data-autocomplete-minimum-characters': 1,
+# 	},
+	
+# 	# This will set the data-widget-maximum-values attribute on the
+# 	# widget container element, and will be set to
+# 	# yourlabs.Widget.maximumValues (jQuery handles the naming
+# 	# conversion).
+# 	widget_attrs={
+# 	    # 'data-widget-maximum-values': 4,
+# 	    'class': 'modern-style',
+#     },	
+# )
